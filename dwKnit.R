2b69f6a098d35c6e9322feeec8905b4379940f7b
Daniel Wollschlaeger <dwoll@kuci.org>
2020-12-01 15:45:59 +0100

    fix knitr errors

--- dwKnit.R
+++ dwKnit.R
@@ -1,61 +1,4 @@
 ## TODO R bug 15012 is fixed -> use gsub() instead of external sed
-## knit one input Rmd file to one output md file
-dwKnit <- function(fIn, fOut,
-                   markdEngine=c("kramdown", "redcarpet", "pandoc"),
-                   siteGen=c("jekyll", "nanoc", "none")) {
-    markdEngine <- match.arg(markdEngine)
-    siteGen     <- match.arg(siteGen)
-
-    require(knitr)
-    require(stringr)
-
-    ## set some directories and get base file name
-    dirTmp <- "../tmp"
-    dirMd  <- "../md"
-    dirR   <- "../R"
-    fName  <- basename(tools::file_path_sans_ext(fIn))
-
-    ## configure knitr chunk options
-    knitr::opts_chunk$set(cache.path=file.path("../tmp/cache/"),
-                            fig.path=file.path("../content/assets/figure/"),
-                                tidy=FALSE, message=FALSE, warning=FALSE, comment=NA)
-
-    ## configure hooks for different types of markdown output
-    knitr::render_markdown(strict=FALSE)
-
-    ## for Jekyll + Redcarpet2, as well as for nanoc + pandoc,
-    ## code blocks can start with knitr's default fenced code block ```r, otherwise:
-    if((siteGen == "jekyll") && (markdEngine == "kramdown")) {
-        ## for Jekyll + kramdown, code blocks need to start with {% highlight r %}
-        knitr::render_jekyll()
-    } else if((siteGen == "nanoc") && (markdEngine == "kramdown")) {
-        ## for nanoc + kramdown, fenced code blocks need to start with ~~~ r
-        hook.t <- function(x, options) stringr::str_c("\n\n~~~\n", x, "~~~\n\n")
-        hook.r <- function(x, options) { 
-            stringr::str_c("\n\n~~~ ", tolower(options$engine), "\n", x, "~~~\n\n")
-        }
-    } else if((siteGen == "nanoc") && (markdEngine == "redcarpet")) {
-        ## for nanoc + Redcarpet2, fenced code blocks need to start with ```language-r
-        hook.t <- function(x, options) stringr::str_c("\n\n```\n", x, "```\n\n")
-        hook.r <- function(x, options) { 
-            stringr::str_c("\n\n```language-", tolower(options$engine), "\n", x, "```\n\n")
-        }
-    }
-
-    ## apply newly defined hooks
-    if(exists("hook.t") && exists("hook.r")) {
-        knitr::knit_hooks$set(source=hook.r, output=hook.t, warning=hook.t,
-                              error=hook.t, message=hook.t)
-    }
-
-    ## add GitHub-Links, knit to markdown, post-process, and extract R code
-    gitHubLinks(fIn, paste(dirTmp, "/", fName, "Tmp.Rmd", sep=""))
-    knitr::knit(     paste(dirTmp, "/", fName, "Tmp.Rmd", sep=""),
-                     paste(dirMd,  "/", fName, ".md",     sep=""))
-    postProcess(     paste(dirMd,  "/", fName, ".md",     sep=""), fOut,
-                markdEngine, siteGen)
-    knitr::purl(fIn, paste(dirR,   "/", fName, ".R",  sep=""))
-}
 
 ## add GitHub links to the bottom of a post
 gitHubLinks <- function(fIn, fOut) {
@@ -167,14 +110,66 @@ sanitizeMath <- function(fIn, markdEngine=c("kramdown", "redcarpet", "pandoc")) 
 }
 
 ## get arguments and call knit
-args        <- commandArgs(TRUE)
+args <- commandArgs(TRUE)
 ## input Rmd file
-inputFile   <- args[1]
+fIn <- args[1]
 ## output md file
-outputFile  <- args[2]
+fOut <- args[2]
 ## which markdown engine wille be used?
 markdEngine <- args[3]
 ## which static site generator will be used?
 siteGen     <- args[4]
 
-dwKnit(inputFile, outputFile, markdEngine, siteGen)
+markdEngine <- match.arg(markdEngine, choices=c("kramdown", "redcarpet", "pandoc"))
+siteGen     <- match.arg(siteGen,     choices=c("jekyll", "nanoc", "none"))
+
+## knit one input Rmd file to one output md file
+require(knitr)
+require(stringr)
+
+## set some directories and get base file name
+dirTmp <- "../tmp"
+dirMd  <- "../md"
+dirR   <- "../R"
+fName  <- basename(tools::file_path_sans_ext(fIn))
+
+## configure knitr chunk options
+knitr::opts_chunk$set(cache.path=file.path("../tmp/cache/"),
+                      fig.path=file.path("../content/assets/figure/"),
+                      tidy=FALSE, message=FALSE, warning=FALSE, comment=NA)
+
+## configure hooks for different types of markdown output
+knitr::render_markdown(strict=FALSE)
+
+## for Jekyll + Redcarpet2, as well as for nanoc + pandoc,
+## code blocks can start with knitr's default fenced code block ```r, otherwise:
+if((siteGen == "jekyll") && (markdEngine == "kramdown")) {
+    ## for Jekyll + kramdown, code blocks need to start with {% highlight r %}
+    knitr::render_jekyll()
+} else if((siteGen == "nanoc") && (markdEngine == "kramdown")) {
+    ## for nanoc + kramdown, fenced code blocks need to start with ~~~ r
+    hook.t <- function(x, options) stringr::str_c("\n\n~~~\n", x, "~~~\n\n")
+    hook.r <- function(x, options) { 
+        stringr::str_c("\n\n~~~ ", tolower(options$engine), "\n", x, "~~~\n\n")
+    }
+} else if((siteGen == "nanoc") && (markdEngine == "redcarpet")) {
+    ## for nanoc + Redcarpet2, fenced code blocks need to start with ```language-r
+    hook.t <- function(x, options) stringr::str_c("\n\n```\n", x, "```\n\n")
+    hook.r <- function(x, options) { 
+        stringr::str_c("\n\n```language-", tolower(options$engine), "\n", x, "```\n\n")
+    }
+}
+
+## apply newly defined hooks
+if(exists("hook.t") && exists("hook.r")) {
+    knitr::knit_hooks$set(source=hook.r, output=hook.t, warning=hook.t,
+                          error=hook.t, message=hook.t)
+}
+
+## add GitHub-Links, knit to markdown, post-process, and extract R code
+gitHubLinks(fIn, paste(dirTmp, "/", fName, "Tmp.Rmd", sep=""))
+knitr::knit(     paste(dirTmp, "/", fName, "Tmp.Rmd", sep=""),
+                 paste(dirMd,  "/", fName, ".md",     sep=""))
+postProcess(     paste(dirMd,  "/", fName, ".md",     sep=""), fOut,
+                 markdEngine, siteGen)
+knitr::purl(fIn, paste(dirR,   "/", fName, ".R",  sep=""))
